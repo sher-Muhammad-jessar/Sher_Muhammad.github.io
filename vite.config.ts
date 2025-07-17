2cb4704d39a1079283f36cc0ef559b3673ea72cc
Sher Muhammed <154434498+sher-Muhammad-jessar@users.noreply.github.com>
2025-07-17 08:49:17 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { tempo } from "tempo-devtools/dist/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'Sher_Muhammad.github.io'
+  base: '/Sher_Muhammad.github.io/'
   optimizeDeps: {
     entries: ["src/main.tsx", "src/tempobook/**/*"],
   },
